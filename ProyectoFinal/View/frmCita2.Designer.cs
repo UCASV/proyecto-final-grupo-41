ca6b083ffa43a3c57e4715f8f949093d8553ddab
ChrMlg <00110219@uca.edu.sv>
2021-06-25 10:01:08 -0600

    Testing cita 2, Problems found in class Vacunacion

--- ProyectoFinal/View/frmCita2.Designer.cs
+++ ProyectoFinal/View/frmCita2.Designer.cs
@@ -30,20 +30,157 @@ namespace ProyectoFinal.View
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCita2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDUI = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHora = new System.Windows.Forms.TextBox();
+            this.cmbLugar = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.cmbGestor = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(75, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DUI:";
+            // 
+            // txtDUI
+            // 
+            this.txtDUI.Location = new System.Drawing.Point(128, 37);
+            this.txtDUI.Name = "txtDUI";
+            this.txtDUI.PlaceholderText = "12345678-0";
+            this.txtDUI.Size = new System.Drawing.Size(163, 23);
+            this.txtDUI.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(57, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Fecha:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(65, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Hora:";
+            // 
+            // txtHora
+            // 
+            this.txtHora.Location = new System.Drawing.Point(128, 126);
+            this.txtHora.Name = "txtHora";
+            this.txtHora.PlaceholderText = "HH:MM";
+            this.txtHora.Size = new System.Drawing.Size(163, 23);
+            this.txtHora.TabIndex = 10;
+            // 
+            // cmbLugar
+            // 
+            this.cmbLugar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLugar.FormattingEnabled = true;
+            this.cmbLugar.Location = new System.Drawing.Point(128, 170);
+            this.cmbLugar.Name = "cmbLugar";
+            this.cmbLugar.Size = new System.Drawing.Size(161, 23);
+            this.cmbLugar.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(58, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Lugar:";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(128, 261);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(117, 45);
+            this.btnAceptar.TabIndex = 17;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // cmbGestor
+            // 
+            this.cmbGestor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGestor.FormattingEnabled = true;
+            this.cmbGestor.Location = new System.Drawing.Point(128, 216);
+            this.cmbGestor.Name = "cmbGestor";
+            this.cmbGestor.Size = new System.Drawing.Size(161, 23);
+            this.cmbGestor.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(51, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Gestor:";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(128, 83);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.PlaceholderText = "YYYY/MM/DD";
+            this.txtFecha.Size = new System.Drawing.Size(163, 23);
+            this.txtFecha.TabIndex = 18;
             // 
             // frmCita2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 399);
+            this.ClientSize = new System.Drawing.Size(362, 361);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.cmbGestor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbLugar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHora);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDUI);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmCita2";
             this.Text = "Cita2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDUI;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHora;
+        private System.Windows.Forms.ComboBox cmbLugar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ComboBox cmbGestor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFecha;
     }
 }
--- ProyectoFinal/View/frmCita2.cs
+++ ProyectoFinal/View/frmCita2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoFinal.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,61 @@ namespace ProyectoFinal.View
         {
             InitializeComponent();
         }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            using (var db = new ProyectoFinalContext())
+            {
+                cmbGestor.DataSource = db.Gestors.ToList();
+                cmbGestor.DisplayMember = "Nombre";
+                cmbGestor.ValueMember = "Id";
+
+                cmbLugar.DataSource = db.CentroVacunacions.ToList();
+                cmbLugar.DisplayMember = "Nombre";
+                cmbLugar.ValueMember = "Id";
+
+                bool validaciones =
+                txtDUI.Text.Length == 10 &&
+                txtFecha.Text.Length == 10 &&
+                txtHora.Text.Length == 5;
+
+                if (validaciones)
+                {
+                    Gestor Gref = (Gestor)cmbGestor.SelectedItem;
+                    CentroVacunacion CVref = (CentroVacunacion)cmbLugar.SelectedItem;
+
+                    // Mediante las variables de referencia se obtienen los datos almacenados en la base
+                    Gestor Gdb = db.Set<Gestor>()
+                        .SingleOrDefault(g => g.Id == Gref.Id);
+
+                    CentroVacunacion CVdb = db.Set<CentroVacunacion>()
+                        .SingleOrDefault(c => c.Id == CVref.Id);
+
+                    Usuario Udb = db.Set<Usuario>()
+                        .SingleOrDefault(u => u.Dui == txtDUI.Text);
+
+                    // Se guardan los datos de la cita (se agenda la cita)
+                    Citum c = new Citum(txtFecha.Text, txtHora.Text, Gdb.Id, Udb.Id, CVdb.Id);
+                    db.Add(c);
+                    db.SaveChanges();
+
+                    // Mensaje de confirmacion
+                    MessageBox.Show("Cita agendada exitosamente!", "Cita1", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Regresando al formulario de prechequeo
+                    frmPrechequeo ventana = new frmPrechequeo();
+                    this.Hide();
+                    ventana.ShowDialog();
+                }
+                else
+                    MessageBox.Show("Los datos ingresados no son validos!", "Cita1", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void frmCita1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
+    
+
